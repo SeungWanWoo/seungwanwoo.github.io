f1b6ee745caf15afa3502441bf55792c97568712
bitcamp <seungwan.woo94@gmail.com>
2019-08-03 17:17:04 +0900

    2019.08.03 index.html ohora.html ver 0.3.1
    fullpage 제거 후 버튼 누르면 해당 섹션으로 이동하도록 변경,
    프로젝트 부분 제작 시작 -> 카드형태로 만들 예정
    프로젝트마다 각각 페이지 제작 예정(Ex; ohora.html)

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 $(document.body).ready(() => {
     new WOW().init();
+
+    $("#menu li").click(function() {
+        var offset = $($(this).attr('data-target')).offset();
+        $("html, body").animate({
+              scrollTop: offset.top
+        }, 1000);
+    });
 });
+
+$('#moreOhora').click((e) => {
+    e.preventDefault();
+    location.href = "ohora.html";
+})
+
