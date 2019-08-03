57a68619af95ab7812db5ee8c86792391a70e830
bitcamp <seungwan.woo94@gmail.com>
2019-08-03 14:57:38 +0900

    2019.08.03 index.html ver0.3.0
    fullpage.js를 뺐습니다. 보다 역동적인 홈페이지를 만들고 싶어서 fullpage.js 를 빼는 대신에 wow.js로 보다 역동적인 페이지로 바꿨습니다.
    한페이지 형식에서 fixed된 메뉴를 클릭하면 다른 페이지로 넘어가도록 변경할 예정입니다.

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
 $(document.body).ready(() => {
     new WOW().init();
-    var myFullpage = new fullpage('#fullpage', {
-        licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
-        anchors: ['firstPage', 'secondPage', 'thirdPage'],
-        continuousVertical: true,
-        navigation: true,
-        navigationPosition: 'right',
-        navigationTooltips: ['Home', 'About', 'Project'],
-        responsiveWidth: 1100,
-        scrollOverflow: true,
-        afterLoad: function(anchorLink, index) {
-        },
-        onLeave: function(origin, destination, direction) {
-        }
-    });
 });
