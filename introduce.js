29b7c05e396426cbe9d39a610b2d9111e012f15e
bitcamp <seungwan.woo94@gmail.com>
2019-12-18 15:46:52 +0900

    2019.12.18 introduce.html 하이브리드웹화 완료 footer 디자인ver1 완료 1. 모바일 상에서도 깨지지 않도록 미디어태그를 활용하여 수치 재입력 완료 2. footer 디자인 ver1 완료 마음에 안들면 언제든 갈아 끼울 수 있음 3. 다음 할 것 : 애니메이션 적용

--- introduce.js
+++ introduce.js
@@ -1,3 +1,13 @@
 $(document.body).ready(() => {
     AOS.init();
 });
+
+$('#mymail').click((e) => {
+    e.preventDefault();
+    location.href = "mailto:seungwan.woo94@gmail.com";
+});
+
+$('#mygit').click((e) => {
+    e.preventDefault();
+    location.href = "https://github.com/SeungWanWoo";
+});
